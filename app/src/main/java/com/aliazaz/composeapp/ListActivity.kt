16776f1f46056a411123cf0bc6236a34d904a5b9
AliAzazAlam <ali.azaz.alam@hotmail.com>
2023-07-04 18:11:15 +0500

    Updated list activity

--- app/src/main/java/com/aliazaz/composeapp/ListActivity.kt
+++ app/src/main/java/com/aliazaz/composeapp/ListActivity.kt
@@ -3,16 +3,17 @@ package com.aliazaz.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -20,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aliazaz.composeapp.ui.theme.FirstComposeAppTheme
+import kotlinx.coroutines.launch
 
 class ListActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,26 +40,61 @@ class ListActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ListApp() {
     val lemonadeMapping = arrayListOf(
         Pair(R.drawable.lemon_tree, "Tap a lemon tree to select a lemon"),
         Pair(R.drawable.lemon_squeeze, "Keep tapping the lemon to squeeze it"),
         Pair(R.drawable.lemon_drink, "Tap the lemonade to drink it"),
+        Pair(R.drawable.lemon_restart, "Tap the empty glass to start again"),
+        Pair(R.drawable.lemon_tree, "Tap a lemon tree to select a lemon"),
+        Pair(R.drawable.lemon_squeeze, "Keep tapping the lemon to squeeze it"),
+        Pair(R.drawable.lemon_drink, "Tap the lemonade to drink it"),
         Pair(R.drawable.lemon_restart, "Tap the empty glass to start again")
     )
 
-    LazyColumn {
-        items(lemonadeMapping) {
-            ItemView(itemData = it)
+    val lazyListState = rememberLazyListState()
+    val showScrollToTopButton by remember {
+        derivedStateOf {
+            lazyListState.firstVisibleItemIndex > 0
         }
     }
+    val coroutineScope = rememberCoroutineScope()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        LazyColumn(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            state = lazyListState,
+            contentPadding = PaddingValues(15.dp)
+        ) {
+            items(lemonadeMapping) {
+                ItemView(itemData = it)
+            }
+        }
+
+        ScrollToTopButton(
+            flag = showScrollToTopButton,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(5.dp)
+                .size(80.dp)
+                .clickable {
+                    coroutineScope.launch {
+                        lazyListState.animateScrollToItem(
+                            0
+                        )
+                    }
+                }
+        )
+    }
+
 }
 
 @Composable
 fun ItemView(itemData: Pair<Int, String>) {
-    Card(modifier = Modifier.padding(10.dp)) {
+    Card {
         Column {
             Image(
                 painter = painterResource(itemData.first),
@@ -75,6 +112,17 @@ fun ItemView(itemData: Pair<Int, String>) {
                 style = MaterialTheme.typography.body1
             )
         }
+    }
+}
+
+@Composable
+fun ScrollToTopButton(flag: Boolean, modifier: Modifier) {
+    if (flag) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_up_arrow),
+            contentDescription = null,
+            modifier = modifier
+        )
     }
 }
 
